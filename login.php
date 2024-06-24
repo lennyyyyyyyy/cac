d8a2313634a48aa72dd5a9027b65f1f79f743ada
Devin Lim <devinlim2025@gmail.com>
2024-06-24 16:46:02 -0400

    login test

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
+<?php
     session_start();
     // Change this to your connection info.
     $DATABASE_HOST = 'sql5.freesqldatabase.com';
@@ -17,14 +17,14 @@
     $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
     if ( mysqli_connect_errno() ) {
         // If there is an error with the connection, stop the script and display the error.
-        echo 'Failed to connect to MySQL: ' . mysqli_connect_error()
-        exit();
+        echo('Failed to connect to MySQL: ' . mysqli_connect_error());
+        exit('Failed to connect to MySQL: ' . mysqli_connect_error());
     }
     // Now we check if the data from the login form was submitted, isset() will check if the data exists.
     if ( !isset($_POST['username'], $_POST['password']) ) {
         // Could not get the data that should have been sent.
-        echo 'Please fill both the username and password fields!'
-        exit();
+        echo 'Please fill both the username and password fields!';
+        exit('Please fill both the username and password fields!');
     }
     // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
     if ($stmt = $con->prepare('SELECT id, password FROM login WHERE username = ?')) {
@@ -58,7 +58,7 @@
 
         $stmt->close();
     }
-    ?>
+?>
 </body>
 </html>
 
