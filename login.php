f3d9fea7929222849a23dff5eb3a2e6e03501649
Devin Lim <devinlim2025@gmail.com>
2024-06-24 16:36:35 -0400

    test login page

---
+++ login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    session_start();
+    // Change this to your connection info.
+    $DATABASE_HOST = 'sql5.freesqldatabase.com';
+    $DATABASE_USER = 'sql5715625';
+    $DATABASE_PASS = 'et38IIz7ek';
+    $DATABASE_NAME = 'sql5715625';
+    // Try and connect using the info above.
+    $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+    if ( mysqli_connect_errno() ) {
+        // If there is an error with the connection, stop the script and display the error.
+        echo 'Failed to connect to MySQL: ' . mysqli_connect_error()
+        exit();
+    }
+    // Now we check if the data from the login form was submitted, isset() will check if the data exists.
+    if ( !isset($_POST['username'], $_POST['password']) ) {
+        // Could not get the data that should have been sent.
+        echo 'Please fill both the username and password fields!'
+        exit();
+    }
+    // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+    if ($stmt = $con->prepare('SELECT id, password FROM login WHERE username = ?')) {
+        // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+        $stmt->bind_param('s', $_POST['username']);
+        $stmt->execute();
+        // Store the result so we can check if the account exists in the database.
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+            $stmt->bind_result($id, $password);
+            $stmt->fetch();
+            // Account exists, now we verify the password.
+            // Note: remember to use password_hash in your registration file to store the hashed passwords.
+            if ($_POST['password'] ===  $password) {
+                // Verification success! User has loggedin!
+                // Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server.
+                session_regenerate_id();
+                $_SESSION['loggedin'] = TRUE;
+                $_SESSION['name'] = $_POST['username'];
+                $_SESSION['id'] = $id;
+                header('Location: index.html');
+            } else {
+                // Incorrect password
+                echo 'Incorrect username and/or password!';
+            }
+        } else {
+            // Incorrect username
+            echo 'Incorrect username and/or password!';
+        }
+
+        $stmt->close();
+    }
+    ?>
+</body>
+</html>
+
