05a19f698456992bf2bd18562191d8a90376e5ec
3chospirits <evandan403@gmail.com>
2024-10-11 16:25:48 -0400

    added signup page + error messages for login/signup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,28 +1,111 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 // import { ReactComponent as Waves } from "./wave-haikei.svg"
 import "./index.css"
 
 export default function Login() {
+    const [formData, setFormData] = useState({ // temporary storage for entered data
+        username: '',
+        password: ''
+    });
+    
+    const [errors, setErrors] = useState({}); // displays errors
+    const [submitted, setSubmitted] = useState(false);
+    
+    const handleChange = (e) => { // called when one field is changed
+        const { name, value } = e.target;
+    
+        setFormData({ // updates temporary storage
+          ...formData,
+          [name]: value,
+        });
+    
+        if (submitted) {
+          validateField(name, value);
+        }
+    };
+
+    const validateField = (name, value) => { // checks for errors for one field at a time
+        let error = '';
+    
+        if (name === 'username' && !value) {
+          error = 'Username is required.';
+        }
+    
+        if (name === 'password' && !value) {
+            error = 'Password is required.';
+        }
+    
+        setErrors((prevErrors) => ({ // Update error state for the specific field
+          ...prevErrors,
+          [name]: error,
+        }));
+    };
+
+    const handleSubmit = (e) => { // called when entire form is submitted
+        e.preventDefault();
+        setSubmitted(true);
+    
+        const newErrors = validateForm();
+    
+        if (Object.keys(newErrors).length === 0) { // if no errors, send data to backend
+          // TODO: Send signup info to back end and redirect <---------------------------------------
+        }
+    };
+    
+    const validateForm = () => { // checks entire form for errors
+        const newErrors = {};
+    
+        if (!formData.username) {
+          newErrors.username = 'Username is required.';
+        }
+
+        if (!formData.password) {
+          newErrors.password = 'Password is required.';
+        }
+    
+        setErrors(newErrors);
+        return newErrors;
+    };
+
     return (<>
         <div class="img-header">
-            <img class="overlay-img" src="leaves.png" alt=""></img>
+            <img class="overlay-img" src="login-bg.png" alt=""></img>
             <h1 class="overlay-header">Login</h1>
         </div>
 
-        <form>
+        <form onSubmit={handleSubmit}>
             <div class="form-single-row">
                 <div class="form-item-single">
                     <label>Username</label>
-                    <input type="text" placeholder="Username"></input>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                    />
                 </div>
+                {errors.username && (
+                    <span className="error-msg">{errors.username}</span>
+                )}
             </div>
+
             <div class="form-single-row">
                 <div class="form-item-single">
                     <label>Password</label>
-                    <input type="password" placeholder="Password"></input>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
                 </div>
+                {errors.password && (
+                    <span className="error-msg">{errors.password}</span>
+                )}
             </div>
-            <div class="form-single-row centered">
+            <div class="form-submit-row centered">
                 <input type="submit" value="Login"></input>
             </div>
         </form>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,8 +1,160 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 import "./index.css"
 
 export default function Signup() {
+    const [formData, setFormData] = useState({ // temporarily saving the values user enters
+        username: '',
+        password: '',
+        passwordConf: '',
+    });
+    
+    const [errors, setErrors] = useState({}); // used to display list of errors
+    const [submitted, setSubmitted] = useState(false); // see if user has submitted at least once
+    
+    const handleChange = (e) => { // everytime user makes changes to one field, this is called
+        const { name, value } = e.target;
+    
+        setFormData({ // update the temporary storage
+          ...formData,
+          [name]: value,
+        });
+    
+        if (submitted) { // will display errors if submitted 1+ times
+          validateField(name, value);
+        }
+    };
+
+    const validateField = (name, value) => { // checks entered data for one field for errors
+        let error = '';
+    
+        if (name === 'username' && !value) {
+          error = 'Username is required.'; // error msgs say what each if statement does
+        }
+    
+        if (name === 'password') {
+          if (!value) {
+            error = 'Password is required';
+          } else if (value.length < 5 || !/\d/.test(value) || !/[a-zA-Z]/.test(value)) {
+            error = 'Password did not meet requirements.';
+          }
+        }
+    
+        if (name === 'passwordConf' && value !== formData.password) {
+          error = 'Passwords do not match.';
+        }
+    
+        setErrors((prevErrors) => ({ // Update error state for the that field
+          ...prevErrors,
+          [name]: error,
+        }));
+    };
+
+    const handleSubmit = (e) => { // called when submit button pressed
+        e.preventDefault();
+        setSubmitted(true);
+    
+        const newErrors = validateForm();
+    
+        if (Object.keys(newErrors).length === 0) { // if no errors, send data to back end
+          // TODO: Send signup info to back end and redirect <---------------------------------------
+        }
+    };
+    
+    const validateForm = () => { // checks the entire form
+        const newErrors = {};
+    
+        if (!formData.username) { // error msgs say what these do
+          newErrors.username = 'Username is required.';
+        }
+
+        if (!formData.password) {
+          newErrors.password = 'Password is required.';
+        }
+        else if (formData.password.length < 5 || !/\d/.test(formData.password) || !/[a-zA-Z]/.test(formData.password)) {
+          newErrors.password = 'Password does not meet requirements.';
+        }
+
+        if (!formData.passwordConf) {
+            newErrors.passwordConf = 'Password confirmation is required.'
+        }
+        else if (formData.passwordConf !== formData.password) {
+          newErrors.passwordConf = 'Passwords do not match.';
+        }
+    
+        setErrors(newErrors);
+        return newErrors;
+    };
+
+    // the rest is just html
     return (<>
-        <h1>Sign up</h1>
+        <div class="img-header">
+            <img class="overlay-img" src="signup-bg.png" alt=""></img>
+            <h1 class="overlay-header">Signup</h1>
+        </div>
+
+        <form onSubmit={handleSubmit}>
+            <div class="form-single-row">
+                <div class="form-item-single">
+                    <label>Username</label>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                    />
+                </div>
+                {errors.username && (
+                    <span className="error-msg">{errors.username}</span>
+                )}
+            </div>
+
+            <div class="form-single-row">
+                <div class="form-item-single">
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                </div>
+                {errors.password && (
+                    <span className="error-msg">{errors.password}</span>
+                )}
+            </div>
+            <div className="form-single-row">
+                <div className="form-item-single">
+                    <span className="form-paragraph">
+                        <p className="form-info">Password must consist of</p>
+                        <ul className="form-list">
+                            <li className="form-info">At least 1 number</li>
+                            <li className="form-info">At least 1 letter</li>
+                            <li className="form-info">At least 5 characters</li>
+                        </ul>
+                    </span>
+                </div>
+            </div>
+
+            <div class="form-single-row">
+                <div class="form-item-single">
+                    <label>Confirm Password</label>
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="passwordConf"
+                        value={formData.passwordConf}
+                        onChange={handleChange}
+                    />
+                </div>
+                {errors.passwordConf && (
+                    <span className="error-msg">{errors.passwordConf}</span>
+                )}
+            </div>
+            <div class="form-submit-row centered">
+                <input type="submit" value="Signup"></input>
+            </div>
+        </form>
     </>);
 }
